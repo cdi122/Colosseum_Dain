632fd937735e79a8abb7827853696ee3c134751e
DI <cdi122@nate.com>
2022-09-22 13:22:50 +0900

    Serverutil 클래스 추가

--- app/src/main/java/com/example/colosseum_dain/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colosseum_dain/utils/ServerUtil.kt
@@ -2,5 +2,12 @@ package com.example.colosseum_dain.utils
 
 class ServerUtil {
 
+    companion object {
+        val BASE_URL = "http://54.180.52.26"
+
+        fun postRequestLogin() {}
+
+    }
+
 
 }
